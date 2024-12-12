37a552dcca1b3b6119997ab53b1a76837d962809
“shaiDahari <shaidahari@gmail.com>
2024-12-13 01:23:06 +0200

    Updated shaiDahari.java with new lines

--- shaiDahari.java
+++ shaiDahari.java
@@ -1,7 +1,21 @@
-public class shaiDahari{
-	public static void main(String[] args){
-		for(int i = 1; i<=10; i++){
-			System.out.println(i + ") Shai Dahari");
-		}
-	}
+public class shaiDahari {
+    public static void main(String[] args) {
+        String[] lyrics = {
+            "I just can't I just can't I just can't control my feet",
+            "I just can't I just can't I just can't control my feet",
+            "Sunshine Don't blame it on the moonlight Don't blame it on the good times Blame it on the boogie",
+            "Don't blame it on the sunshine Don't blame it on the moonlight Don't, on the good times Blame it on the boogie",
+            "This magic music grooves me That dirty rhythm fools me The Devil's gotten to me through this dance",
+            "I'm full of funky fever A fire burns inside me Boogie's got me in a super trance",
+            "Don't blame it on the sunshine Don't blame it on the moonlight Don't blame it on the good times Blame it on the boogie (Don't you blame it)",
+            "Sunshine Don't blame it on the moonlight Don't blame it on the good times Blame it on the boogie Ow",
+            "woo (Moonlight) yeah (Good times) mmm (Boogie) you just got to",
+            "yeah (Moonlight) (Good times) good times (Boogie)",
+            "Don't you blame it (sunshine) You just got to (moonlight) You just want to (good times) Yeah, oh (boogie) Blame it on yourself (sunshine) Ain't nobody's fault (moonlight) But yours and that boogie (good times) All, all night long (boogie)",
+            "Can't stop that boogie (sunshine) Ain't nobody's fault (moonlight) But yours and that boogie (good times) Dancin' all night long (boogie) Blame it on yourself (sunshine) Ain't nobody's fault (moonlight) But yours and that funky Funky boogie, boogie, (woo) yeah"
+        };
+        for (int i = 0; i < lyrics.length; i++) {
+            System.out.println((i + 11) + ") " + lyrics[i]);
+        }
+    }
 }
