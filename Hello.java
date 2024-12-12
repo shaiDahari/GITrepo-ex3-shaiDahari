343f4ea34ecc2f2cfa43f61bd301d00785812413
“shaiDahari <shaidahari@gmail.com>
2024-12-13 00:50:05 +0200

    Updated Hello.java with new lines

--- Hello.java
+++ Hello.java
@@ -1,7 +1,29 @@
-public class Hello{
-	public static void main(String[] args){
-		for(int i = 1; i<=10; i++){
-			System.out.println("Hello World ..."+i);
-		}
-	}
+public class Hello {
+    public static void main(String[] args) {
+		println("Blame It on the Boogie")
+        String[] lyrics = {
+			"Hee, hee, hee",
+            "My baby's always dancin' And wouldn't be a bad thing
+			But I don't get no lovin' And that's no lie",
+            "We spent the night in Frisco At every kind of disco
+			From that night I kissed our love goodbye",
+			"Don't blame it on the sunshine Don't blame it on the moonlight
+			Don't blame it on the good times Blame it on the boogie",
+			"Don't blame it on the sunshine Don't blame it on the moonlight
+			Don't blame it on the good times Blame it on the boogie",
+			"That nasty boogie bugs me But somehow it has drugged me
+			Spellbound rhythm gets me on my feet",
+			"I've changed my life completely I've seen the lightning leave me
+			And my baby just can't take her eyes off me",
+			"Don't blame it on the sunshine Don't blame it on the moonlight
+			Don't blame it on the good times Blame it on the boogie",
+			"Don't you blame it on the sunshine Don't blame it on the moonlight
+			Don't blame it on the good times Blame it on the boogie Woo",
+			"I just can't I just can't I just can't control my feet",
+            "I just can't I just can't (yeah) I just can't control (woo) my feet"
+        };
+        for (int i = 0; i < lyrics.length; i++) {
+            System.out.println((i + 1) + ") " + lyrics[i]);
+        }
+    }
 }
